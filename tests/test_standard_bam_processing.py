99c203382db5927bd19c9f00e32169b604c8ceb9
ionox0 <ionox0@gmail.com>
2019-09-04 19:02:47 -0400

    remove unneeded travis steps now done by tox

--- tests/test_standard_bam_processing.py
+++ tests/test_standard_bam_processing.py
@@ -3,6 +3,7 @@
 
 """Tests for `standard_bam_processing` package."""
 
+import os
 import pytest
 import subprocess
 
@@ -13,6 +14,13 @@ def test_cwltool_workflow():
     cmd = ['cwltool', '--preserve-environment', 'PATH', 'standard_bam_processing.cwl', 'example_input.json']
     rc = subprocess.check_call(cmd)
     print(rc)
+
+    assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001.fastq.gz_trimming_report.txt')
+    assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md.bai')
+    assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md.bam')
+    assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md.metrics')
+    assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md_abra_fm_bqsr.bai')
+    assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md_abra_fm_bqsr.bam')
 
 
 pytest.main()
