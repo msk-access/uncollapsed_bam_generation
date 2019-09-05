c9d51331f6290ac13d58359e4a7ede4fe63a3965
ionox0 <ionox0@gmail.com>
2019-09-05 12:37:01 -0400

    setup and teardown tests

--- install_data.cmd
+++ install_data.cmd
@@ -6,11 +6,15 @@ fileid=19Ncjp89IWcIq1XuurFkGu2GZUU0Zzrna
 
 filename=test-data.tar.gz
 
-# Skip if already have test datas
+# Skip if already have test data
 [[ -f $filename ]] && exit 0
+
+echo "HERE"
 
 curl -c ./cookie -s -k -L "https://drive.google.com/uc?export=download&id=$fileid" > /dev/null
 
 curl -k -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
 tar -xzvf $filename
+
+rm test-data.tar.gz
--- tests/test_standard_bam_processing.py
+++ tests/test_standard_bam_processing.py
@@ -5,14 +5,58 @@
 
 import os
 import pytest
+import shutil
 import subprocess
+
+
+RESULT_FILES = [
+    # Cookie file from test data download step
+    'cookie',
+    'test_patient_1_test_investigator_sample_1_R1_001.fastq.gz_trimming_report.txt',
+    'test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md.bai',
+    'test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md.bam',
+    'test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md_abra_fm_bqsr.bai',
+    'test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md_abra_fm_bqsr.bam',
+    'test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md.metrics',
+]
+
+def setup_module():
+    """
+    Run the workflow with cwltool
+    """
+    print('SETUP')
+
+
+    cmd = [
+        'cwltool',
+        '--preserve-environment',
+        'PATH',
+        'standard_bam_processing.cwl',
+        'example_input.json'
+    ]
+
+    rc = subprocess.check_call(cmd)
+
+    print('cwltool return code: ' + str(rc))
+
+
+def teardown_module():
+    """
+    Delete output files
+    """
+    print('TEARDOWN')
+
+    for file in RESULT_FILES:
+        if os.path.exists(file):
+            os.remove(file)
+
+    #shutil.rmtree('test_data_access')
 
 
 def test_trimming_results():
     """
     Trimgalore results should exist
     """
-
     assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001.fastq.gz_trimming_report.txt')
 
 
@@ -34,16 +78,6 @@ def test_md_metrics():
     Metrics from MarkDuplicates should exist
     """
     assert os.path.exists('test_patient_1_test_investigator_sample_1_R1_001_val_1_srt_md.metrics')
-
-
-def test_run_cwltool_workflow():
-    """
-    Run the workflow with cwltool
-    """
-    cmd = ['cwltool', '--preserve-environment', 'PATH', 'standard_bam_processing.cwl', 'example_input.json']
-    rc = subprocess.check_call(cmd)
-
-    print('cwltool return code: ' + str(rc))
 
 
 pytest.main()
