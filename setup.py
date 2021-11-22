6626b2ad762bc708fbc5b577778eff6d72e4a1ed
Ronak Shah <rons.shah@gmail.com>
2021-11-22 17:24:59 -0500

    Bump version: 0.1.2 → 1.1.0

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/msk-access/uncollapsed_bam_generation',
-    version='0.1.2',
+    version='1.1.0',
     zip_safe=False,
 )
